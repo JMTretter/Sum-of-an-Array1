b77d5f1cb58b87e552f3c70dd26716fe7407b2cd
jmtretter.jb <jmtretter.jb@hotmail.com>
2017-12-13 19:33:20 -0600

    Push to GitHub

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 3, 5, 7, 9, 11, 13, 15 };
+            int sum = 0;
+
+            for (int i = 1; i < myArray.Length; i++)
+            {
+                //Console.WriteLine(myArray[i]);
+                sum = myArray[i] + sum;
+                Console.WriteLine(sum);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
